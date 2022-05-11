09cf82f113ffc8ae682a408e9ef39e12b78229e9
Zach Hughes <zach.hughes79@gmail.com>
2022-05-11 18:39:09 -0400

    Intial commit. Created app, components, and styling.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//1. Create a new React app.
+import React from "react";
+import ReactDOM from "react-dom";
+import { directive } from "@babel/types";
+import App from "./components/App";
+
+
+ReactDOM.render(
+    <App />, document.getElementById("root")
+);
