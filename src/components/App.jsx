ef7d73cc0bd45c9f27786fd3d993ebc807478330
Zach Hughes <zach.hughes79@gmail.com>
2022-05-13 19:28:17 -0400

    using arrow functions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,19 +4,20 @@ import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes"
 
-function createNote(note){
-    return <Note
-    key ={note.key}
-    title = {note.title}
-    content = {note.content}
-    />;
-}
+
+
 
 function App (){
     return (
     <div>
         <Heading />
-        {notes.map(createNote)}
+        {notes.map(note => (
+            <Note
+                key ={note.key}
+                title = {note.title}
+                content = {note.content}
+            />
+            ))}
         <Footer />
     </div>
     );
